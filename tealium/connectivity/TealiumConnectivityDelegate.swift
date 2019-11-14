30bb5aab3af5b2b56c12b3b0b3bb1a0622e7385b
Craig Rouse <craig.rouse@tealium.com>
2019-11-14 11:49:09 +0000

    Revert "Updated sample apps (#165)" (#166)
    This reverts commit adcf42f39ebab4e0f613eac096865f763a747727.

---
+++ tealium/connectivity/TealiumConnectivityDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  TealiumConnectivityDelegate.swift
+//  tealium-swift
+//
+//  Created by Craig Rouse on 16/5/18.
+//  Copyright © 2018 Tealium, Inc. All rights reserved.
+//
+
+import Foundation
+
+public protocol TealiumConnectivityDelegate: class {
+
+    /// Called when network connection type has changed￼.
+    ///
+    /// - Parameter connectionType: `String` containing the current connection type (wifi, cellular)
+    func connectionTypeChanged(_ connectionType: String)
+
+    /// Called when network connectivity is lost.
+    func connectionLost()
+
+    /// Called when network connectivity is restored.
+    func connectionRestored()
+}
