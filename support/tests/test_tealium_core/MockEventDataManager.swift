6d09eef4cf50599c9db0dec960b3caeda27b6ba5
craigrouse <craig.rouse@gmail.com>
2020-05-14 09:59:41 +0100

    wip

--- support/tests/test_tealium_core/MockEventDataManager.swift
+++ support/tests/test_tealium_core/MockEventDataManager.swift
@@ -10,88 +10,87 @@ import Foundation
 @testable import TealiumCore
 
 class MockEventDataManager: EventDataManagerProtocol {
-    var sessionDataBacking = [String : Any]()
-    
-    var allEventData: [String : Any] {
+    var sessionDataBacking = [String: Any]()
+
+    var allEventData: [String: Any] {
         get {
-           ["all": "eventdata"]
+            ["all": "eventdata"]
         }
         set {
             self.add(data: newValue, expiration: .forever)
         }
     }
-    
-    var allSessionData: [String : Any] {
-       ["all": "sessiondata"]
+
+    var allSessionData: [String: Any] {
+        ["all": "sessiondata"]
     }
-    
+
     var minutesBetweenSessionIdentifier: TimeInterval = 1.0
-    
+
     var secondsBetweenTrackEvents: TimeInterval = 1.0
-    
+
     var sessionId: String? {
         get {
-           "testsessionid"
+            "testsessionid"
         }
         set {
             self.add(data: ["sessionId": newValue!], expiration: .session)
         }
     }
-    
-    var sessionData: [String : Any] {
+
+    var sessionData: [String: Any] {
         get {
-          ["session": "data"]
+            ["session": "data"]
         }
         set {
             sessionDataBacking += newValue
         }
     }
-    
+
     var sessionStarter: SessionStarterProtocol {
         MockTealiumSessionStarter()
     }
-    
+
     var tagManagementIsEnabled: Bool = true
-    
-    func add(data: [String : Any], expiration: Expiration) {
-        
+
+    func add(data: [String: Any], expiration: Expiration) {
+
     }
-    
+
     func add(key: String, value: Any, expiration: Expiration) {
 
     }
-    
+
     func addTrace(id: String) {
-        
+
     }
-    
+
     func delete(forKeys: [String]) {
-        
+
     }
-    
+
     func delete(forKey key: String) {
-        
+
     }
-    
+
     func deleteAll() {
-        
+
     }
-    
+
     func leaveTrace() {
-        
+
     }
-    
+
     func refreshSessionData() {
-        
+
     }
-    
+
     func sessionRefresh() {
-        
+
     }
-    
+
     func startNewSession(with sessionStarter: SessionStarterProtocol) {
-        
+
     }
-    
-    
+
 }
