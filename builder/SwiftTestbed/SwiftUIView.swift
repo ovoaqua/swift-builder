7c570efcc4b6823368c54c1bbbd3136987e362e7
Jonathan Wong <34286815+jonathanswong@users.noreply.github.com>
2020-09-01 08:06:57 -0700

    Fixed redundancy in sample app. (#233)

--- builder/SwiftTestbed/SwiftUIView.swift
+++ builder/SwiftTestbed/SwiftUIView.swift
@@ -13,50 +13,53 @@ struct SwiftUIView: View {
     @State var text: String = "Enter Trace ID"
     
     var body: some View {
-        VStack(alignment: .center) {
-                    Text("Swift Demo App")
-                    Spacer()
-                    Button(action: {
-                        print("Track View")
-                        TealiumHelper.shared.trackView(title: "iOS View", data: nil)
-                    },
+        NavigationView {
+            VStack(alignment: .center, spacing: 15.0) {
+                Button(action: {
+                    print("Track View")
+                    TealiumHelper.shared.trackView(title: "iOS View", data: nil)
+                },
                        label: {
                         Text("Track View")
-                    }).padding(.bottom)
-                    
-                    Button(action: {
-                         print("Track Event")
-                        TealiumHelper.shared.track(title: "iOS Event", data: nil)
-                    },
-                    label: {
-                         Text("Track Event")
-                    }).padding(.bottom)
-            
-                    Button(action: {
-                        TealiumHelper.shared.toggleConsentStatus()
-                    },
-                    label: {
-                         Text("Toggle consent status")
-                    }).padding(.bottom)
-            
-                    TextField("Trace ID", text: $text) {
-                        TealiumHelper.shared.joinTrace(self.text)
-                    }.multilineTextAlignment(.center)
-                    .border(Color.black)
-                        .frame(width: 150.0, height: nil, alignment: .center)
-                    .padding(.bottom)
-            
-                    Button(action: {
-                        TealiumHelper.shared.leaveTrace()
-                        self.text = "Enter Trace ID"
-                    },
-                    label: {
-                         Text("Leave Trace")
-                    }).padding(.bottom)
+                })
                 
-                    Spacer()
-                    Spacer()
-                    Spacer()
+                Button(action: {
+                    print("Track Event")
+                    TealiumHelper.shared.track(title: "iOS Event", data: nil)
+                },
+                       label: {
+                        Text("Track Event")
+                })
+                
+                Button(action: {
+                    TealiumHelper.shared.toggleConsentStatus()
+                },
+                       label: {
+                        Text("Toggle consent status")
+                })
+                TextField("Trace ID", text: $text) {
+                    TealiumHelper.shared.joinTrace(self.text)
+                }.multilineTextAlignment(.center)
+                    .border(Color.black)
+                    .overlay(RoundedRectangle(cornerRadius: 4.0)
+                        .stroke(lineWidth: 2.0)
+                        .foregroundColor(.black))
+                    .frame(width: 150.0, height: nil, alignment: .center)
+                Button(action: {
+                    TealiumHelper.shared.leaveTrace()
+                    self.text = "Enter Trace ID"
+                },
+                       label: {
+                        Text("Leave Trace")
+                })
+                
+                Button(action: {
+                    TealiumHelper.shared.tealium?.disable()
+                }, label: {
+                    Text("Stop Tealium")
+                })
+            }
+            .navigationBarTitle("Swift Demo App")
         }
     }
 }
