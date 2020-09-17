bef11af72ee06152a1150779248816c58357f5b9
Craig Rouse <craig.rouse@tealium.com>
2020-09-18 00:48:08 +0100

    Bug/mt 332/podspec (#236)
    * podspec fix
    
    * removed resources
    
    * version numbers

--- tealium/core/TealiumConstants.swift
+++ tealium/core/TealiumConstants.swift
@@ -16,7 +16,7 @@ public enum Dispatchers {}
 
 public enum TealiumValue {
     public static let libraryName = "swift"
-    public static let libraryVersion = "2.0.0"
+    public static let libraryVersion = "2.0.1"
     // This is the current limit for performance reasons. May be increased in future
     public static let maxEventBatchSize = 10
     public static let defaultMinimumDiskSpace: Int32 = 20_000_000
